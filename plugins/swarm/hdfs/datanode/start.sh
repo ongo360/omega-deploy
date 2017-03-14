e7d5746fa3b0e0004b9a18504a00896ef12ee61d
uniseraph <zhengtao.wuzt@gmail.com>
2017-03-14 22:56:37 +0800

    using external network

--- plugins/swarm/hdfs/datanode/start.sh
+++ plugins/swarm/hdfs/datanode/start.sh
@@ -2,5 +2,8 @@
 docker run -ti --rm -v $(pwd):$(pwd) \
 	-v /var/run/docker.sock:/var/run/docker.sock \
         -e DOCKER_HOST=unix:///var/run/docker.sock  \
-        -w $(pwd)  docker/compose:1.9.0 \
+        -e NETWORK=${NETWORK} \
+        -w $(pwd) \
+        docker/compose:1.9.0 \
+        -p myhadoop-${DEFAULT_IP} \
         up -d
--- plugins/swarm/hdfs/namenode/start.sh
+++ plugins/swarm/hdfs/namenode/start.sh
@@ -2,5 +2,7 @@
 docker run -ti --rm -v $(pwd):$(pwd) \
 	-v /var/run/docker.sock:/var/run/docker.sock \
         -e DOCKER_HOST=unix:///var/run/docker.sock  \
+        -e NETWORK=${NETWORK} \
         -w $(pwd)  docker/compose:1.9.0 \
+        -p hdfs \
         up -d
