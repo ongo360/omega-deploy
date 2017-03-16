e9f7dd6b9f0bc81f15b0fc320e9695e12ad5a044
uniseraph <zhengtao.wuzt@gmail.com>
2017-03-17 00:22:26 +0800

    using bridge

--- plugins/swarm/mesos/start.sh
+++ plugins/swarm/mesos/start.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
+
+HOST_IP=$(ifconfig eth0 | grep inet | awk '{{print $2}}')
+
 docker run -ti --rm \
-   -v $(pwd):$(pwd)  \ 
-   -v /var/run/docker.sock:/var/run/docker.sock   \
-   -w $(pwd) \  
+   -v $(pwd):$(pwd) \
+   -v /var/run/docker.sock:/var/run/docker.sock \
+   -w $(pwd) \
    -e DOCKER_HOST=unix:///var/run/docker.sock  \
    -e ZK_URL=${ZK_URL} \
+   -e HOST_IP=${HOST_IP} \
    docker/compose:1.9.0   \
-   up $@
+   up -d $*
