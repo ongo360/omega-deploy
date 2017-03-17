a5f6190f3329c8f45456e2ae542bb28b3c17e881
uniseraph <zhengtao.wuzt@gmail.com>
2017-03-17 17:54:48 +0800

    add consul plugin

---
+++ plugins/swarm/consul/start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+HOST_IP=$(ifconfig eth0 | grep inet | awk '{{print $2}}')
+
+docker run -ti --rm \
+   -v $(pwd):$(pwd) \
+   -v /var/run/docker.sock:/var/run/docker.sock \
+   -w $(pwd) \
+   -e DOCKER_HOST=unix:///var/run/docker.sock  \
+   -e HOST_IP=${HOST_IP} \
+   docker/compose:1.9.0   \
+   up -d $*
