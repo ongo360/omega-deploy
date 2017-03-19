6ecd288e608ec1f938e5eb23b37014ba7a6329f5
uniseraph <zhengtao.wuzt@gmail.com>
2017-03-19 16:07:02 +0800

    using LOCAL IP

--- plugins/swarm/hdfs/datanode/start.sh
+++ plugins/swarm/hdfs/datanode/start.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+LOCAL_IP=$(ifconfig eth0 | grep inet | awk '{{print $2}}')
+
 docker run -ti --rm -v $(pwd):$(pwd) \
 	-v /var/run/docker.sock:/var/run/docker.sock \
         -e DOCKER_HOST=unix:///var/run/docker.sock  \
         -e NETWORK=${NETWORK} \
         -w $(pwd) \
         docker/compose:1.9.0 \
-        -p myhadoop-${DEFAULT_IP} \
+        -p myhadoop-${LOCAL_IP} \
         up -d
